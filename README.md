8793619532bcc5109989a332006dd4349a8e1dc2
yanosz <yanosz@gmx.net>
2016-02-21 18:29:38 +0100

    Zwischenstand

---
+++ README.md
@@ -0,0 +1,4 @@
+# HowTo
+## Worum geht's?
+
+Dieses Projekt enthält einige Konfigurationsdateien, die beispielhaft für einen Freifunk-Node verwendet werden können.
--- freifunk/vpn/up.sh
+++ freifunk/vpn/up.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Fuege eine Default-Route mit Protokoll 43 der Freifunk-Tabelle (66) hinzu
-ip route add default dev $DEV table 66 proto static 
+ip route add default dev tun0 table 66 proto static 
