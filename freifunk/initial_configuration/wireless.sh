06f7b766b6821790b7eef47cca1d206a4243940a
yanosz <yanosz@gmx.net>
2016-10-23 18:28:43 +0200

    Support for

--- freifunk/initial_configuration/wireless.sh
+++ freifunk/initial_configuration/wireless.sh
@@ -21,3 +21,30 @@ uci -q batch <<EOF
         set wireless.wifi_mesh.mcast_rate='12000'
         commit wireless
 EOF
+# Is there a second one?
+if [ "$(uci get wireless.radio1)" ]; then
+ uci -q batch <<EOF
+        delete wireless.radio1.disabled                 # WLAN Einschalten
+        delete wireless.@wifi-iface[1]                  # OpenWRT-Default WLAN loeschen
+        set wireless.radio1.channel='36'                 # Funkeinstellungen
+        set wireless.radio1.htmode='HT20'
+        set wireless.radio1.country='DE'
+        
+        set wireless.wifi_freifunk5='wifi-iface'         # 1. WLAN: Accesspoint
+        set wireless.wifi_freifunk5.device='radio1'
+        set wireless.wifi_freifunk5.network='freifunk'
+        set wireless.wifi_freifunk5.mode='ap'
+        set wireless.wifi_freifunk5.ssid='Freifunk'
+        
+        set wireless.wifi_mesh5='wifi-iface'             # 2. WLAN: Ad-Hoc Mesh
+        set wireless.wifi_mesh5.device='radio1'
+        set wireless.wifi_mesh5.network='mesh babel_mesh'
+        set wireless.wifi_mesh5.mode='adhoc'
+        set wireless.wifi_mesh5.ssid='42:42:42:42:42:42'
+        set wireless.wifi_mesh5.bssid='42:42:42:42:42:42'
+        set wireless.wifi_mesh5.mcast_rate='12000'
+        commit wireless
+EOF
+fi
+
+
