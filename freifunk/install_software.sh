8a9efa4c7bdc317f462faea8c2f6aae4a1a389a4
yanosz <yanosz@gmx.net>
2016-10-23 18:29:26 +0200

    Fix missing packages

--- freifunk/install_software.sh
+++ freifunk/install_software.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PKGS="ip openvpn-polarssl babeld fastd ebtables kmod-ebtables-ipv4 owipcalc batctl haveged"
+PKGS="ip openvpn-polarssl babeld fastd ebtables kmod-ebtables-ipv4 owipcalc batctl haveged kmod-nf-nathelper-extra kmod-pptp ppp-mod-pptp"
 
 echo "Installing: $PKGS"
 opkg update
