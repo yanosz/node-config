b51e8358f805870a9443d370055c7515ecc598be
Jan 'yanosz' Lühr <lede@yanosz.net>
2017-02-27 18:49:14 +0100

    Workground for LEDE Bug FS#433

--- freifunk/install_software.sh
+++ freifunk/install_software.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-PKGS="ip-full openvpn-mbedtls  babeld fastd owipcalc batctl haveged kmod-nf-nathelper-extra kmod-pptp ppp-mod-pptp  ebtables kmod-ebtables-ipv4"
+PKGS="ip-full openvpn-mbedtls  babeld fastd owipcalc batctl haveged kmod-nf-nathelper-extra kmod-pptp ppp-mod-pptp"
 
-echo "Installing: $PKGS"
+echo "Installing: $PKGS ebtables kmod-ebtables-ipv4"
 opkg update
+#Ignore errors installing ebtables due to Lede Bug FS#433
 opkg install $PKGS
+if [ "$?" != "0" ];then
+  exit 1
+fi
+opkg install ebtables kmod-ebtables-ipv4
+exit 0;
